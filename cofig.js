8e2eb2455be4721ae5938d980bf9740f9b8e1cfe
Bhanuprakash648 <bhanuprakash_sanikommu@srmap.edu.in>
2024-02-16 22:30:28 +0530

    new

---
+++ cofig.js
@@ -0,0 +1,2 @@
+export const PORT=5555;
+export const mongoDBURL='mongodb+srv://bp7386:Bhanu648@bp.5bpxfjk.mongodb.net/?retryWrites=true&w=majority';
---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import {PORT,mongoDBURL} from "./cofig.js";
+import mongoose from 'mongoose';
+import {Book} from './models/bookModel.js'; 
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+const app=express(); 
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET','POST','PUT','DELETE','PATCH'],
+    allowedHeaders: ['Content-Type'],
+}));
+
+app.get('/',(request ,response) => {
+    console.log(request);
+    return response.status(234).send("welcome To mern stack tutorials");
+});
+
+app.use('/books',booksRoute);
+mongoose.connect(mongoDBURL).then(()=>{
+    console.log("app connected to database");
+    app.listen(PORT,()=>{
+        console.log(`APP is listening to port: ${PORT}`); 
+    });
+}).catch((error)=>{
+    console.log(error);
+});
