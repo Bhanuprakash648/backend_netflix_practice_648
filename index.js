a583141cd680ebabfee58fd4326e5bce67e782ca
Bhanuprakash648 <113592731+Bhanuprakash648@users.noreply.github.com>
2024-02-17 11:36:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app=express(); 
 app.use(express.json());
-app.listen(PORT,()=>{
-        console.log(`APP is listening to port: ${PORT}`); 
+app.listen(5000,()=>{
+        console.log("APP is listening to port: 5000"); 
 });
